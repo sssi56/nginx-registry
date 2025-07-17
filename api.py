1917593759f99ed4065d70de87f4b34b22af6d9f
work <sidorov30@yandex.ru>
2025-07-17 11:38:18 +0500

    Переименовал

---
+++ api.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import os
+
+app = FastAPI()
+
+# Настраиваем работу с шаблонами
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Путь к директории с файлами
+FILE_DIRECTORY = "/opt/container"
+
+@app.get("/")
+async def read_root(request: Request):
+    try:
+        # Получаем список файлов
+        files = os.listdir(FILE_DIRECTORY)
+    except FileNotFoundError:
+        files = []
+    
+    return templates.TemplateResponse(
+        "index.html", 
+        {
+            "request": request, 
+            "files": files
+        }
+    )
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
